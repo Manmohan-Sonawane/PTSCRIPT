753282b820da73328a8230a83f8101bdea649a6d
Manmohan-Sonawane <129599687+Manmohan-Sonawane@users.noreply.github.com>
2026-05-19 13:16:30 +0530

    Update lock.sql

--- lock.sql
+++ lock.sql
@@ -1,2 +1,13 @@
-select sid , serial#,(last_call_et/60) last_call_et_mins,wait_time,(seconds_in_wait/60) waiting_mins   from gv$session
-where sid in ( select sid from gv$lock where block=1);
+SELECT
+    sid,
+    serial#,
+    (last_call_et / 60) AS last_call_et_mins,
+    wait_time,
+    (seconds_in_wait / 60) AS waiting_mins,
+    CLIENT_IDENTIFIER
+FROM
+    gv$session
+WHERE
+    sid IN (SELECT sid FROM gv$lock WHERE block = 1)
+/
+
