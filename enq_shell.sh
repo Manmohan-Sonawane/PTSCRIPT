6a89b9ed371acbcfeb1bc1ee9d76395c69b80e08
Manmohan-Sonawane <129599687+Manmohan-Sonawane@users.noreply.github.com>
2026-05-19 13:08:04 +0530

    Add SQL script for enqueue wait events script added
    Added SQL script to retrieve session wait events related to enqueue.

--- enq_shell.sh
+++ enq_shell.sh
@@ -41,3 +41,16 @@ fi
 # Return the exit status from SQL*Plus...
 #----------------------------------------------------------------------------
 exit 0 > /dev/null 2>&1
+
+
+
+
+
+script:- 
+
+
+SQL> !cat /oci_stage/clover/crontab/alert/enq.sql
+set lines 400 pages 400
+set colsep '|'
+select inst_id,sid,event,p1,p2,p3,wait_time,seconds_in_wait from gv$session_wait where event like 'enq%' order by 8 desc;
+
