65ebe3afa80ad6b47fc1fcd75763bf84a7717208
Manmohan-Sonawane <129599687+Manmohan-Sonawane@users.noreply.github.com>
2026-05-19 13:17:15 +0530

    Update blk.sql

--- blk.sql
+++ blk.sql
@@ -1,6 +1,4 @@
-set lines 300 pages 400
-col module for a50
-col event for a30
-SELECT sid,inst_id,module, serial#,sql_id, blocking_session_status, blocking_session,event,LAST_CALL_ET
+SELECT sid,inst_id,module, serial#,sql_id, blocking_session_status, blocking_session, CLIENT_IDENTIFIER, event,LAST_CALL_ET
 FROM   gv$session
-WHERE  blocking_session IS NOT NULL;
+WHERE  blocking_session IS NOT NULL
+/
